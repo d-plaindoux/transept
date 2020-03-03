7c64753331378d3ed6b63b837912bb00b573421c
d-plaindoux <d.plaindoux@free.fr>
2020-03-03 07:18:49 +0100

    Review response denotation thanks to standard result adt

--- lib/transept_core/response.ml
+++ lib/transept_core/response.ml
@@ -1,16 +1,12 @@
-type ('s, 'a) t =
-  | Success of 's * 'a * bool
-  | Failure of 's * bool
-
 module Basic = struct
-  type nonrec ('s, 'a) t = ('s, 'a) t
+  type ('s, 'a) t = ('s * 'a * bool, 's * bool) result
 
-  let success (s, a, c) = Success (s, a, c)
+  let success v = Ok v
 
-  let failure (s, c) = Failure (s, c)
+  let failure v = Error v
 
   let fold response fSuccess fFailure =
     match response with
-    | Success (s, a, c) -> fSuccess (s, a, c)
-    | Failure (s, c) -> fFailure (s, c)
+    | Ok v -> fSuccess v
+    | Error v -> fFailure v
 end
--- lib/transept_core/response.mli
+++ lib/transept_core/response.mli
@@ -1,3 +1,1 @@
-type ('s, 'a) t
-
-module Basic : Transept_specs.RESPONSE with type ('s, 'a) t = ('s, 'a) t
+module Basic : Transept_specs.RESPONSE
